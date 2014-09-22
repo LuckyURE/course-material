71341357bcf2ddd15b6ef85f3ceabd7d84f6522e
Joshua M. Andrews <joshua@qcsitter.com>
2014-09-22 15:19:48 -0500

    solut 013

---
+++ exercices/013/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 15:08:51 2014
+
+@author: joshua_andrews
+"""
+
+from sys import argv
+
+if len(argv) < 2:
+    print('usage: python3 solution.py PARAM')
+else:
+    print(argv[1])
